0154e8bc21ca2f5e135b3fa284c1e44eeb35924c
Zack Beauchamp <44363095+ZackToTheB@users.noreply.github.com>
2019-12-16 18:50:13 +0000

    Updated date

--- progress.js
+++ progress.js
@@ -1,5 +1,5 @@
-var start = new Date("April 29, 2019 00:00:00");
-var end = new Date("June 29, 2019 00:00:00");
+var start = new Date("December 14, 2019 00:00:00");
+var end = new Date("January 13, 2019 00:00:00");
 
 var c; //current progress
 var t; //time left
